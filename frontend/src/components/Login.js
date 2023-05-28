4932cb77865898a949e2f51831f81c20b75000b5
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-05-28 13:02:52 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   }, []);
 
   const handleLogin = async () => {
-    let result = await fetch("http://34.67.206.143:5000/api/login", {
+    let result = await fetch("/api/login", {
       method: "post",
       body: JSON.stringify({ email, password }),
       headers: {
